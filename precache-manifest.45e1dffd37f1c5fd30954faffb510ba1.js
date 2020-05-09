8eb29cd20c59d601508464c48bcd88b33868a40e
Richard Zilahi <richard.zilahi@salesframe.com>
2020-05-10 01:09:07 +0300

    Updates

---
+++ precache-manifest.45e1dffd37f1c5fd30954faffb510ba1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d56fd54f940e4c47aa02bbf9334e7543",
+    "url": "/react-magyarorszag-map/index.html"
+  },
+  {
+    "revision": "42743cfeb01c9d70347d",
+    "url": "/react-magyarorszag-map/static/css/main.37a19c1a.chunk.css"
+  },
+  {
+    "revision": "bcaeb63198b67e0a2563",
+    "url": "/react-magyarorszag-map/static/js/2.77d7a1f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-magyarorszag-map/static/js/2.77d7a1f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e05cbafcfa53d64fc75b",
+    "url": "/react-magyarorszag-map/static/js/3.8951bf6f.chunk.js"
+  },
+  {
+    "revision": "42743cfeb01c9d70347d",
+    "url": "/react-magyarorszag-map/static/js/main.df600dd1.chunk.js"
+  },
+  {
+    "revision": "c6005092bee7f1eee61e",
+    "url": "/react-magyarorszag-map/static/js/runtime-main.f320f2b4.js"
+  }
+]);
